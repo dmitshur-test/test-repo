0694b97fe1e9d271674d019b174c1f9790ca755f
Dmitri Shuralyov <shurcooL@gmail.com>
2016-05-05 13:37:11 -0700

    Add lib package for testing.

---
+++ lib/lib.go
@@ -0,0 +1,26 @@
+package lib
+
+type baseClient struct{}
+
+func (b *baseClient) Foo() error {
+	return nil
+}
+
+type commandable struct{}
+
+func (c *Client) Del(key string) error {
+	return nil
+}
+
+type Client struct {
+	baseClient
+	commandable
+}
+
+func (c *Client) Bar() error {
+	return nil
+}
+
+type AnotherCmd struct {
+	commandable
+}
